4c0e68269421f4542f1096ae5f3eeb20eb5ed878
mihajlo1604 <mihajlo_sk95@hotmail.com>
2025-10-21 22:21:41 +0200

    update carousel card

--- src/components/carousel-cards/carousel-card-1/carousel-card-1.jsx
+++ src/components/carousel-cards/carousel-card-1/carousel-card-1.jsx
@@ -1,8 +1,6 @@
 "use client";
 
-import Link from "next/link";
-import Grid from "@mui/material/Grid";
-import Button from "@mui/material/Button";
+
 import { useTranslation } from "react-i18next";
 
 // GLOBAL CUSTOM COMPONENTS
@@ -27,24 +25,8 @@ export default function CarouselCard1({
   const { t } = useTranslation();
   
   return <StyledRoot>
-      <Grid container spacing={3} alignItems="center">
-        <Grid className="grid-item" size={{
-        md: 6,
-        xs: 12
-      }}>
-          <h1 className="title">{t(title)}</h1>
-          <p className="description">{t(description)}</p>
-
-        </Grid>
-
-        <Grid size={{
-        md: 6,
-        xs: 12
-      }}>
-          <div className="img-wrapper">
-            <LazyImage fill src={imgUrl} alt={t(title)} sizes="(max-width: 768px) 100vw, 100vw" />
-          </div>
-        </Grid>
-      </Grid>
+      <div className="img-wrapper">
+        <LazyImage fill src={imgUrl} alt={t(title)} sizes="(max-width: 768px) 100vw, 100vw" />
+      </div>
     </StyledRoot>;
 }
